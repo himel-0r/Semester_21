c9a9c1c5fceca2f36e1726890fc0c8c57bcbef00
himel-0r <himelchandraroy@gmail.com>
2023-03-03 04:09:25 +0600

    abc.cpp is a shit

---
+++ Pseudoes/abc.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    deque<int>dq;
+
+    // eikhankar function gular kaaj shikhte hobe
+    // then oigula implement korar cheshta korte hbe
+    // DSA etto moja
+    // baki gulate kono moja nai keno?
+    // vallagena
+    
+}
