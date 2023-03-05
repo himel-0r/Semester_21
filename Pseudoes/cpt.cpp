3b63052db7fe82a817cb94445f462bcc275c7f40
himel-0r <himelchandraroy@gmail.com>
2023-03-05 22:22:18 +0600

    pt added

---
+++ Pseudoes/cpt.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define int ll
+#define vi vector<ll>
+#define vvi vector<vi>
+#define all(vec) (vec).begin(), (vec).end()
+#define endl '\n'
+#define sp " "
+
+void solveit()
+{
+    
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // freopen ("inp.txt", "r", stdin);
+    // freopen ("out.txt", "w", stdout);
+
+    int t = 1;
+    cin >> t;
+
+    for (int tt = 1; tt <= t; tt++)
+    {
+        // cout << "Case " << tt << ": ";
+        solveit();
+    }
+}
