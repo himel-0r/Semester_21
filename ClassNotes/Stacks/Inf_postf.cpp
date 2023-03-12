e51ba6eed14d8bd83772b268673297cad3514675
himel-0r <himelchandraroy@gmail.com>
2023-03-13 01:51:24 +0600

    dghj;lgcmnj;jhg

---
+++ ClassNotes/Stacks/Inf_postf.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // freopen ("input.txt", "r", stdin);
+    freopen ("output.txt", "w", stdout);
+
+    string s = "(1+2)*3";
+    string t = "";
+
+    stack<char> st;
+
+    for (int i=0; i<s.size(); i++) {
+        char x = s[i];
+
+        if (x == '(')
+            st.push(x);
+        else if (x == '+' || x == '-' || x == '*' || x == '/') {
+            if (x == '*' || x == '/') {
+                while (st.top() == '*' || st.top()=='/') {
+                    t += st.top();
+                    st.pop();
+                }
+                st.push(x);
+            }
+            else 
+                st.push(x);
+        }
+        else if (x>='0' && x<='9')
+            t += x;
+        else if (x == ')') {
+            while (st.top() != '(') {
+                t += st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+    }
+
+    while (!st.empty()) {
+        cout << st.top() << endl;
+        t += st.top();
+        st.pop();
+    }
+
+    cout << t << endl;
+}
